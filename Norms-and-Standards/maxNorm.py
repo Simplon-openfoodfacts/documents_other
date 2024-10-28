42f00c9c6b228888ad4a7c9d4d308372b901acad
El-Nouty <69649680+delnouty@users.noreply.github.com>
2024-10-28 12:36:36 +0100

    Create maxNorm.py

---
+++ Norms-and-Standards/maxNorm.py
@@ -0,0 +1,18 @@
+nutritional_values_max = {
+    'energy-kcal_100g': 600,
+    'fat_100g': 20,
+    'saturated-fat_100g': 10,
+    'cholesterol_100g': 300,
+    'carbohydrates_100g': 50,
+    'sugars_100g': 22.5,
+    'fiber_100g': 'N/A',  # Fiber not specified in the values provided
+    'proteins_100g': 20,
+    'salt_100g': 3,
+    'sodium_100g': 1200,
+    'vitamin-c_100g': 80,
+    'potassium_100g': 2000,
+    'calcium_100g': 1200,
+    'iron_100g': 15,
+    'magnesium_100g': 350,
+    'zinc_100g': 10
+}
